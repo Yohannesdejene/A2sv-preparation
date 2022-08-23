a25eb50077e9c5a85697f8200a832b1c8569ebcd
Yohannes Dejene <yohannesdejene23@gmail.com>
2022-08-23 11:40:22 +0300

    insertion sort edited

--- insertionSort.py
+++ insertionSort.py
@@ -5,20 +5,22 @@ import random
 import re
 import sys
 
+def insertionSort1(arr):
+  
+    for x in range(len(arr)-1):
+       target=arr[x+1];
+       y=x
+       while (target < arr[y]) and (y >= 0):
+         arr[y+1] = arr[y]
+         y-=1;
 
-def insertionSort1(n, arr):
-    target = arr[-1]
-    index = n-2
+       arr[y+1] = target
+    # print(*arr)
+    #
+    # 
+    # #check with array=arr
+    # 
+arr=[2,4,5,76,5,4,-5,6,9];
 
-    while (target < arr[index]) and (index >= 0):
-        arr[index+1] = arr[index]
-        print(*arr)
-        index -= 1
-
-    arr[index+1] = target
-    print(*arr)
-
-#check with array=arr
-arr=[2,4,5,76,8,9];
-n=6
-insertionSort1(n, arr)
+insertionSort1(arr)
+print(arr);
